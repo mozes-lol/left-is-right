24202786e81ad4a098421aef7f93e551d66f468c
mozes-lol <johnmarkgarcia164@gmail.com>
2025-11-26 16:13:55 +0800

    Apply game states

--- LeftRightGame/left-right-game/scripts/test.gd
+++ LeftRightGame/left-right-game/scripts/test.gd
@@ -5,8 +5,8 @@ extends Control
 @onready var ready_timer = $ReadyTimer
 
 # 1 - Game
-enum game_state { game_start, game_in_progress, game_finished }
-var current_game_state = game_state.game_start
+enum game_state { start, in_progress, finished }
+var current_game_state = game_state.start
 # 2 - Rounds
 enum round_state { ready, running }
 var current_round_state = round_state.ready
@@ -30,33 +30,39 @@ func get_direction(): # Picks a random direction (left or right) whenever this f
 	return direction[randi() % direction.size()]
 
 func _process(_delta):
-	match current_round_state:
-		round_state.ready:
-			print("Round " + str(round))
-			print("Goal: " + str(required_right_answers))
-			print("Ready...")
-			ready_timer.start()
-			current_round_state = null # Stop the current state from looping and let ready timer do its thing
-		round_state.running: # Within this state, there are two more sub-states:
-			match current_cycle_state:
-				cycle_state.get_direction:
-					check_round_progress()
-					if (current_round_state == round_state.running): # Do not re-run the cycle unless the round state is set to running
-						direction_needed = get_direction()
-						print(direction_needed)
-						current_cycle_state = cycle_state.wait_for_user_input # To the next cycle state
-				cycle_state.wait_for_user_input:
-					if (penalty_is_active == false):
-						# User's answer is correct
-						if ((Input.is_action_just_pressed("game_action_left") and direction_needed == "Left") or (Input.is_action_just_pressed("game_action_right") and direction_needed == "Right")):
-							print_rich ("[color=green]Correct!\n")
-							current_cycle_state = cycle_state.get_direction # Back to the previous cycle state
-							current_right_answers += 1
-						# User's answer is wrong
-						elif ((Input.is_action_just_pressed("game_action_left") and direction_needed == "Right") or (Input.is_action_just_pressed("game_action_right") and direction_needed == "Left")):
-							print_rich ("[color=red]Wrong! Answer again.")
-							penalty_is_active = true
-							penalty_timer.start()
+	match current_game_state:
+		game_state.start:
+			pass
+		game_state.in_progress:
+			match current_round_state:
+				round_state.ready:
+					print("Round " + str(round))
+					print("Goal: " + str(required_right_answers))
+					print("Ready...")
+					ready_timer.start()
+					current_round_state = null # Stop the current state from looping and let ready timer do its thing
+				round_state.running: # Within this state, there are two more sub-states:
+					match current_cycle_state:
+						cycle_state.get_direction:
+							check_round_progress()
+							if (current_round_state == round_state.running): # Do not re-run the cycle unless the round state is set to running
+								direction_needed = get_direction()
+								print(direction_needed)
+								current_cycle_state = cycle_state.wait_for_user_input # To the next cycle state
+						cycle_state.wait_for_user_input:
+							if (penalty_is_active == false):
+								# User's answer is correct
+								if ((Input.is_action_just_pressed("game_action_left") and direction_needed == "Left") or (Input.is_action_just_pressed("game_action_right") and direction_needed == "Right")):
+									print_rich ("[color=green]Correct!\n")
+									current_cycle_state = cycle_state.get_direction # Back to the previous cycle state
+									current_right_answers += 1
+								# User's answer is wrong
+								elif ((Input.is_action_just_pressed("game_action_left") and direction_needed == "Right") or (Input.is_action_just_pressed("game_action_right") and direction_needed == "Left")):
+									print_rich ("[color=red]Wrong! Answer again.")
+									penalty_is_active = true
+									penalty_timer.start()
+		game_state.finished:
+			pass
 
 func check_round_progress():
 	if (current_right_answers >= required_right_answers):
